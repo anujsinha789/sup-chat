fce48f2f002ea0cd222dd3fcac56b059ab290be8
Anuj Sinha <50338398+anujsinha789@users.noreply.github.com>
2020-06-30 11:20:13 +0530

    Add files via upload

---
+++ client-js/client.js
@@ -0,0 +1,57 @@
+const socket = io('http://localhost:8000');
+// console.log(window.location.hostname);
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('msg-ip');
+const messageContainer = document.querySelector('.message-screen');
+const serverStatus = document.getElementById('active-status');
+const userList = document.querySelector('active-users');
+
+var names = {};
+
+serverStatus.innerText = 'online'; 
+serverStatus.classList.remove('offline');
+serverStatus.classList.add('online');
+
+var audio = document.getElementById('audio');
+
+
+const append = (message,position) =>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position == 'left'){
+        audio.play();
+    }
+    
+}
+
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    if(message != ''){
+        append(`You: ${message}`,'right');
+        socket.emit('send',message);
+        messageInput.value = '';
+    }   
+    
+})
+
+const name = prompt("Enter your name to join!");
+socket.emit('new-user-joined',name);
+
+socket.on('user-joined',name =>{
+    append(`${name} joined the chat`,'right');
+})
+
+socket.on('receive', data =>{
+    append(`${data.name}: ${data.message}`,'left');
+   
+})
+
+socket.on('left', data =>{
+    append(`${name}: left the chat`,'left');
+})
